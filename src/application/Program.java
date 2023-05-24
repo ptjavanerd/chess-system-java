3794ccf281bac3adbd8119c7d953ae20adc49ba6
Carlos <carlos@ifuturo.net>
2023-05-24 09:48:08 +0100

    Starting to omplement Board and Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,10 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Program {
     public static void main(String[] args) {
-        Position set = new Position(5,3);
-        System.out.println(set);
+        Board board = new Board(8,8);
     }
 }
