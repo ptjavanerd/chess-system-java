fcf8e688e6d7e820ae1c7a1142ffbb77ceae10b3
Carlos <carlos@ifuturo.net>
2023-06-08 21:18:53 +0100

    BoardException and defensive programming2

--- src/boardgame/BoardException.java
+++ src/boardgame/BoardException.java
@@ -1,4 +1,10 @@
 package boardgame;
 
-public class BoardExeption extends Exception{
+public class BoardException extends RuntimeException{
+
+    private static final long serialVersionUID = 1L;
+
+    public BoardException(String msg){
+       super(msg);
+    }
 }
