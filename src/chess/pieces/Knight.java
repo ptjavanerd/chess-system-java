309bb547223246c8dfbda415bcb7e62b172f7e45
Carlos <carlos@ifuturo.net>
2023-07-25 23:33:33 +0100

    Special Move Castling

--- src/chess/pieces/Knight.java
+++ src/chess/pieces/Knight.java
@@ -2,10 +2,13 @@ package chess.pieces;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.Color;
 
 public class Knight extends ChessPiece {
+
+
 
     public Knight(Board board, Color color) {
         super(board, color);
